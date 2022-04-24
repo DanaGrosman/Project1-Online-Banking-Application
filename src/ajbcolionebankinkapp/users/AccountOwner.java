ba5c10c5bd9b42fc9f6e53e759e2f944c59c09db
DanaGrosman <grosmandanna@gmail.com>
2022-04-24 23:52:16 +0300

    add constractors in AccountOwner and BankManager classes

--- src/ajbcolionebankinkapp/users/AccountOwner.java
+++ src/ajbcolionebankinkapp/users/AccountOwner.java
@@ -1,5 +1,12 @@
 package ajbcolionebankinkapp.users;
 
+import java.time.LocalDate;
+
 public class AccountOwner extends Person {
+
+	public AccountOwner(String firstName, String lastName, PhoneNumber phoneNumber, LocalDate birthDate) {
+		super(firstName, lastName, phoneNumber, birthDate);
+		// TODO Auto-generated constructor stub
+	}
 
 }
--- src/ajbcolionebankinkapp/users/BankManager.java
+++ src/ajbcolionebankinkapp/users/BankManager.java
@@ -1,5 +1,12 @@
 package ajbcolionebankinkapp.users;
 
+import java.time.LocalDate;
+
 public class BankManager extends AccountOwner {
+
+	public BankManager(String firstName, String lastName, PhoneNumber phoneNumber, LocalDate birthDate) {
+		super(firstName, lastName, phoneNumber, birthDate);
+		// TODO Auto-generated constructor stub
+	}
 
 }
