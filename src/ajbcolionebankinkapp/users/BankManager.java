eed553e8c127ed228b843b98f37ef96c8801c4a8
DanaGrosman <grosmandanna@gmail.com>
2022-04-25 12:04:30 +0300

    update BankManager constrator

--- src/ajbcolionebankinkapp/users/BankManager.java
+++ src/ajbcolionebankinkapp/users/BankManager.java
@@ -4,8 +4,9 @@ import java.time.LocalDate;
 
 public class BankManager extends AccountOwner {
 
-	public BankManager(String firstName, String lastName, PhoneNumber phoneNumber, LocalDate birthDate) {
-		super(firstName, lastName, phoneNumber, birthDate);
+	public BankManager(String firstName, String lastName, PhoneNumber phoneNumber, LocalDate birthDate,
+			Cerdetianls cerdetianls) {
+		super(firstName, lastName, phoneNumber, birthDate, cerdetianls);
 		// TODO Auto-generated constructor stub
 	}
 
