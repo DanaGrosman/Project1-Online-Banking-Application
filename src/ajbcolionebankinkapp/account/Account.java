79aad5aa104ff1d5fd0274402c06011239d23d76
DanaGrosman <grosmandanna@gmail.com>
2022-04-26 11:03:57 +0300

    update activityData class and add history field to account class

--- src/ajbcolionebankinkapp/account/Account.java
+++ src/ajbcolionebankinkapp/account/Account.java
@@ -3,8 +3,10 @@ package ajbcolionebankinkapp.account;
 import ajbcolionebankinkapp.enumaretion.AccountProperties;
 
 public class Account {
+	protected final static int MAX_ACTIVITY_DATA = 100;
 	protected double balance;
 	protected AccountProperties accountProperties;
+	protected ActivityData[] history = new ActivityData[MAX_ACTIVITY_DATA];
 
 	public double getBalance() {
 		return balance;
@@ -13,11 +15,11 @@ public class Account {
 	public void setBalance(double balance) {
 		this.balance = balance;
 	}
-	
+
 	public void setAccountProperties(AccountProperties accountProperties) {
 		this.accountProperties = accountProperties;
 	}
-	
+
 	public AccountProperties getAccountProperties() {
 		return accountProperties;
 	}
--- src/ajbcolionebankinkapp/account/ActivityData.java
+++ src/ajbcolionebankinkapp/account/ActivityData.java
@@ -1,5 +1,22 @@
 package ajbcolionebankinkapp.account;
 
-public class ActivityData {
+import java.time.LocalDateTime;
 
+import ajbcolionebankinkapp.enumaretion.ActivityName;
+
+public class ActivityData {
+	protected ActivityName activityName;
+	protected double balanceChange;
+	protected LocalDateTime timeStamp;
+	protected String info;
+	
+	public ActivityData(ActivityName activityName, double balanceChange, LocalDateTime timeStamp, String info) {
+		this.activityName = activityName;
+		this.balanceChange = balanceChange;
+		this.timeStamp = timeStamp;
+		this.info = info;
+	}
+	
+	
+	
 }
